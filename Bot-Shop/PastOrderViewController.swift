8f2133e2d35727d0e75c486564ad3daafa2e2e99
Conor Andrews <conor.a@btinternet.com>
2021-02-22 18:53:17 +0000

    Bot-Shop | Add basic 2 screen tab nav
    - Adds the possibility of navigating between 2 differently coloured blank pages.

---
+++ Bot-Shop/PastOrderViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PastOrderViewController.swift
+//  Bot-Shop
+//
+//  Created by Conor Andrews on 22/02/2021.
+//
+
+import Foundation
+import UIKit
+
+class PastOrderViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+}
