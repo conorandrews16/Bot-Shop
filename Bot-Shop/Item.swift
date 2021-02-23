c29fdb83e4da28de7226f45ed7c8b247a14a1c57
Conor Andrews <conor.a@btinternet.com>
2021-02-23 12:55:05 +0000

    Bot-Shop | Create Item model, build out basic table view
    - Creates the Item model which will import each product category and assign them a title and image.
    - Creates a table view for the history page.

---
+++ Bot-Shop/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Bot-Shop
+//
+//  Created by Conor Andrews on 23/02/2021.
+//
+
+import Foundation
+import UIKit
+
+struct Item {
+    let title: String
+    let image: UIImage
+}
