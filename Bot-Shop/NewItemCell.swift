a9fa8850b6f4e2f68d5f37e0457dd2dc054111a8
Conor Andrews <conor.a@btinternet.com>
2021-02-23 11:29:28 +0000

    Bot-Shop | Set up basic collection view
    - Create a product catalogue feel by adding squares that will eventually fit a product image or clickable.
    - 10 squares per page.

---
+++ Bot-Shop/NewItemCell.swift
@@ -0,0 +1,13 @@
+//
+//  NewItemCell.swift
+//  Bot-Shop
+//
+//  Created by Conor Andrews on 23/02/2021.
+//
+
+import Foundation
+import UIKit
+
+class NewItemCell: UICollectionViewCell {
+    
+}
